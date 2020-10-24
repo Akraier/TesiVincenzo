d573f1110f284deeda4ad42cfc58c07f4b6891c5
akraier <diodatovincenzo@yahoo.com>
2020-10-24 16:04:40 +0200

    new auto_run

--- auto_run.sh
+++ auto_run.sh
@@ -4,17 +4,18 @@
 
 model="BASIC"
 count=1
+image_size=224
 file="Test_output.txt"
 for (( epochs=5; epochs<=20; epochs+=5)) do
 	for ((batch_size=8; batch_size<=32; batch_size+=8)) do
-		for ((image_size=150;image_size<=250;image_size+=50)) do
-			echo "============================================================" >>$file
-			echo "Testing on epoch: $epochs;batch_size: $batch_size; image_size: $image_size">>$file
-			echo>>$file
-			python main.py -m $model -d DATASET1/realdataset_PREPROCESSED -b $batch_size -i ${image_size}x1 -e $epochs >>$file 
-			echo>>$file
-			echo "********Test $count Ended********">>$file
+		#for ((image_size=150;image_size<=250;image_size+=50)) do
+			echo "============================================================"
+			echo "Testing on epoch: $epochs;batch_size: $batch_size; image_size: $image_size"
+
+			python main.py -m $model -d DATASET1/realdataset_PREPROCESSED -b $batch_size -i ${image_size}x3 -e $epochs
+
+			echo "********Test $count Ended********"
 			((count++))
-		done
+		#done
 	done
 done
