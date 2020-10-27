3eef536599d77903cc2a48a7c8ee23d72f0188b7
akraier <diodatovincenzo@yahoo.com>
2020-10-27 17:24:11 +0100

    limite inferiore batch size aumentato a 24

--- auto_run.sh
+++ auto_run.sh
@@ -6,7 +6,7 @@ model="BASIC"
 
 
 for (( epochs=5; epochs<=20; epochs+=5)) do
-	for ((batch_size=8; batch_size<=32; batch_size+=8)) do
+	for ((batch_size=24; batch_size<=32; batch_size+=8)) do
 		for ((image_size=150;image_size<=250;image_size+=50)) do
 
 
